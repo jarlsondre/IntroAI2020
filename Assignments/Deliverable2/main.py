a4e13eed87d368f71606db3b6e8aa61c5e0f9dc9
Jarl Sondre Bringslid Sæther <jssaethe@stud.ntnu.no>
2020-09-15 10:49:50 +0200

    Added node class

--- Assignments/Deliverable2/main.py
+++ Assignments/Deliverable2/main.py
@@ -1,12 +1,37 @@
 import Map
 import math
 # Implementation of the A* algorithm :)
+
+class Node:
+    
+    def __init__(self):
+        self.g_cost = 0
+        self.h_cost = 0
+        self.f_cost = 0
+        self.status = "open"
+        self.parent = None
+        self.kids = []
+
+    def set_h_cost(self, pos, goal):
+        self.h_cost = heuristic(pos, goal)
+
+    def close_node(self):
+        self.status = "closed"
+
+    def set_g_cost(self, cost):
+        self.g_cost = cost
+
+    def set_f_cost(self):
+        self.f_cost = self.h_cost + self.g_cost
+
+    def set_parent(self, parent):
+        self.parent = parent
+
+    def add_child(self, child):
+        self.kids.append(child)
+
 def A_star_impl():
-
-    # We want a heuristic function. To calculate this we'll use the euclidean distance.
-
-    # placeholder while we're figuring things out
-    return 0
+    pass
 
 def heuristic(node, goal):
     # Calculating the euclidean distance between a node and the goal node:
