c6d0db138e1c6f21647bf39fe0d2b33ab4f946e6
Tomas Gule <Vaagengule@hotmail.com>
2020-09-15 22:21:09 +0200

    #funker ikke

--- Assignments/Deliverable2/main.py
+++ Assignments/Deliverable2/main.py
@@ -1,10 +1,12 @@
 import Map
 import math
+
+
 # Implementation of the A* algorithm :)
 
 class Node:
-    
-    def __init__(self, pos=[0,0]):
+
+    def __init__(self, pos=[0, 0]):
         self.g_cost = 0
         self.h_cost = 0
         self.f_cost = 0
@@ -13,8 +15,8 @@ class Node:
         self.kids = []
         self.pos = pos
 
-    def set_h_cost(self, pos, goal):
-        self.h_cost = heuristic(pos, goal)
+    def set_h_cost(self, goal):
+        self.h_cost = manhattan_distance(self.pos, goal)
 
     def close_node(self):
         self.status = "closed"
@@ -34,41 +36,91 @@ class Node:
     def get_pos(self):
         return self.pos
 
+    def get_g_cost(self):
+        return self.get_g_cost()
+
     def __str__(self):
         pos = "[" + str(self.pos[0]) + ", " + str(self.pos[1]) + "]"
-        return pos 
+        return pos
 
 
 def main():
-    
     # Making the list of nodes:
     opened = []
     closed = []
-    frontier = [] # the nodes we're going to be looking at next, sorted by their heuristic
+    frontier = []  # the nodes we're going to be looking at next, sorted by their heuristic
 
     # Creating a map object:
     myMap = Map.Map_Obj(1)
     myMap.show_map()
 
     # Creating some instances of nodes and testing their positions;
-    startNode = Node(myMap.get_start_pos())
-    goalNode = Node(myMap.get_goal_pos())
-    print("Goal node:", goalNode)
-    print("Start node:", startNode)
 
-    # Adding nodes to list:
+    startNode = Node(myMap.get_start_pos())
+    startNode.set_g_cost(0)
+    startNode.set_h_cost(myMap.get_goal_pos())
+    startNode.set_f_cost()
+    print("Start node:", startNode)
+    print("Goal node:", myMap.get_goal_pos())
+
+    # Adding node to list:
     opened.append(startNode)
     frontier.append(startNode)
 
-    # Testing the distance function:
-    print("Manhattan distance between start and goal: ", manhattan_distance(startNode.get_pos(), goalNode.get_pos()))
+    goal_not_found = True
 
-def manhattan_distance(first_pos, second_pos): # Calculating the distance in how many steps it takes to move there
+    # This loop will run until we find the shortest path
+    while goal_not_found:
+        currentNode = frontier[0]
+
+        print(currentNode)
+
+        # Checking if it´s the goal node:
+        if currentNode.h_cost == 0:
+            print("Hurra, du fant noden på: ", currentNode)
+            break
+
+        # If not, then we have to search further :)
+
+        # Creating children in a plus shape
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if (i == 0) ^ (j == 0):
+                    if myMap.get_cell_value([currentNode.get_pos()[0] + i, currentNode.get_pos()[1] + j]) == 1:
+                        nodeExist = False
+                        for elem in opened:
+                            if elem.pos == [currentNode.get_pos()[0] + i, currentNode.get_pos()[1] + j]:
+                                node = elem
+                                nodeExist = True
+                                break
+
+                        if not nodeExist:
+                            node = Node([currentNode.get_pos()[0] + i, currentNode.get_pos()[1] + j])
+                        if not node in closed:
+                            node.set_parent(currentNode)
+                            currentNode.add_child(node)
+                            # node.set_g_cost(node.parent.get_g_cost() + 1)
+                            node.set_h_cost(myMap.get_goal_pos)
+                            node.set_f_cost()
+                            opened.append(node)
+                            frontier.append(node)
+
+
+        frontier.sort(key=lambda x: x.f_cost)
+
+        frontier.remove(currentNode)
+        closed.append(currentNode)
+        goal_not_found = False
+
+    for elem in startNode.kids:
+        print(elem)
+
+
+def manhattan_distance(first_pos, second_pos):  # Calculating the distance in how many steps it takes to move there
     hor_dist = abs(first_pos[0] - second_pos[0])
     ver_dist = abs(first_pos[1] - second_pos[1])
     return hor_dist + ver_dist
-       
+
 
 if __name__ == "__main__":
     main()
-
